554fd3cbd68a7f7278c50d0a90c4b81dbe169833
Smaragda Kalogeropoulou <smaragda_kal@hotmail.com>
2020-12-12 16:59:16 +0100

    converting to handlebars templating

--- app.js
+++ app.js
@@ -1,25 +1,30 @@
 const express = require('express');
 const app = express();
+const hbs = require('hbs');
+hbs.registerPartials(__dirname + '/views/partials');
 
 app.use(express.static('public'));
 
-app.get('/home', function (request, response) {
-  response.sendFile(__dirname + '/views/index.html');
+app.set('view engine', 'hbs');
+app.set('views', __dirname + '/views');
+
+app.get('/', function (request, response) {
+  response.render('index', { message: 'Hello there', pageTitle: 'Home' });
 });
 
 app.get('/works', function (request, response) {
-  response.sendFile(__dirname + '/views/works.html');
+  response.render('works');
 });
 
 app.get('/gallery', function (request, response) {
-  response.sendFile(__dirname + '/views/gallery.html');
+  response.render('gallery');
 });
 
 app.get('/about', function (request, response) {
-  response.sendFile(__dirname + '/views/about.html');
+  response.render('about');
 });
 
 app.get('*', function (request, response) {
-  response.sendFile(__dirname + '/views/index.html');
+  response.render('index');
 });
 app.listen(3010);
